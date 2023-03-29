29d5683e3e893b9f9c7597559ab6efb3c4034af1
styvens-w <109088269+styvens-w@users.noreply.github.com>
2023-03-29 16:16:31 +0200

    projet fini

--- src/components/Housing/index.jsx
+++ src/components/Housing/index.jsx
@@ -3,26 +3,31 @@ import { useParams } from 'react-router-dom';
 import logements from '../../datas/logements.json';
 import Carrousel from '../Carrousel';
 import Info from '../Info';
+import Error from '../../pages/Error';
 
 function HousingIndex() {
   const { idLogement } = useParams();
   const logement = logements.find(({ id }) => id === idLogement);
 
-  return (
-    <React.Fragment>
-      <Carrousel pictures={logement.pictures} title={logement.title} />
-      <Info
-        id={logement.id}
-        title={logement.title}
-        host={logement.host}
-        location={logement.location}
-        rating={logement.rating}
-        tags={logement.tags}
-        description={logement.description}
-        equipments={logement.equipments}
-      />
-    </React.Fragment>
-  );
+  if (!logement) {
+    return <Error />;
+  } else {
+    return (
+      <React.Fragment>
+        <Carrousel pictures={logement.pictures} title={logement.title} />
+        <Info
+          id={logement.id}
+          title={logement.title}
+          host={logement.host}
+          location={logement.location}
+          rating={logement.rating}
+          tags={logement.tags}
+          description={logement.description}
+          equipments={logement.equipments}
+        />
+      </React.Fragment>
+    );
+  }
 }
 
 export default HousingIndex;
